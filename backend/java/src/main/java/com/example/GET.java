539516f8b8c6ef51d822bf5d18d5b958a33721a4
jamminP <117453101+jamminP@users.noreply.github.com>
2022-11-20 22:28:10 +0900

    myfriend 출력

--- backend/java/src/main/java/com/example/GET.java
+++ backend/java/src/main/java/com/example/GET.java
@@ -35,4 +35,36 @@ public class GET {
                     request.port);
         }
     }
+
+    static void myfriend(Network socket, Request request, Connection con, Statement updatestmt) throws Exception {
+        Statement querystmt; // 친구 데이터 userid를 기준으로 friend_list에 담겨 있는거 받아서 friend_id 전송
+
+        JSONObject myfriend_json = new JSONObject();
+
+        myfriend_json.put("id", request.data.get("id")); // json file에서 load하여 login_json에 저장.
+
+        String UpdateMyData_sql = String.format("select Friend_ID from Friend_List where ID = \"%s\"",
+                request.data.get("Friend_ID"));
+        querystmt = con.createStatement();
+
+        ResultSet friend_result = querystmt.executeQuery(UpdateMyData_sql);
+
+        if (!friend_result.next()) {
+            socket.response(new Response(10, "Not Founded User Friend Data", null),
+                    request.ip, request.port);
+        } else {
+            String sql = String.format(
+                    "select StatusMessage from UserStatus where ID = \"%s\"", // 친구 id를 기반으로 UserStatus에 저장된
+                                                                              // statusMessage 가져옴.
+                    request.data.get("Friend_ID"));
+            querystmt = con.createStatement();
+            ResultSet FriendStatus_result = querystmt.executeQuery(sql);
+            FriendStatus_result.next();
+
+            socket.response(
+                    new Response(200, "OK", new User(friend_result, FriendStatus_result).getJson()),
+                    request.ip,
+                    request.port);
+        }
+    }
 }
